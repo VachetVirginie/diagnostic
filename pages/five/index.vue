d42a391b058ba1bfae98820ecd46b813aa8ee2fd
Vivi <vachetvirginie@gmail.com>
2021-11-20 18:27:18 +0100

    five ok

--- pages/five/index.vue
+++ pages/five/index.vue
@@ -1,29 +1,74 @@
 <template>
-    <section class="text-gray-600 body-font text-center dark:text-primary-light">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap w-full mb-20 flex-col items-center ">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font">Probleme sur le Five.</h1>
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-col text-center w-full mb-20">
+        <h2 class="sm:text-3xl text-2xl text-indigo-500 tracking-widest font-medium title-font mb-1">Problemes sur le Five.</h2>
+      </div>
+      <div class="flex flex-wrap -m-4">
+        <div class="p-4 md:w-1/3">
+          <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+            <div class="flex items-center mb-3">
+              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </div>
+              <h2 class="text-gray-900 text-lg title-font font-medium">Le loader tourne en boucle.</h2>
+            </div>
+            <div class="flex-grow">
+              <p class="leading-relaxed text-base">Une interface est bloquee sur le loader qui tourne en boucle.</p>
+              <a class="mt-3 text-indigo-500 inline-flex items-center"> <NuxtLink to="/boo">Learn More</NuxtLink>
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 md:w-1/3">
+          <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+            <div class="flex items-center mb-3">
+              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+              </div>
+              <h2 class="text-gray-900 text-lg title-font font-medium">HightLight Tv.</h2>
+            </div>
+            <div class="flex-grow">
+              <p class="leading-relaxed text-base">Vous rencontrez un probleme relatif a l'hightLight Tv.</p>
+              <a class="mt-3 text-indigo-500 inline-flex items-center"><NuxtLink to="/boo">Learn More</NuxtLink>
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="p-4 md:w-1/3">
+          <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+            <div class="flex items-center mb-3">
+              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <circle cx="6" cy="6" r="3"></circle>
+                  <circle cx="6" cy="18" r="3"></circle>
+                  <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                </svg>
+              </div>
+              <h2 class="text-gray-900 text-lg title-font font-medium">Trouver une session.</h2>
+            </div>
+            <div class="flex-grow">
+              <p class="leading-relaxed text-base">Vous n'arrivez pas a trouver une session.</p>
+              <a class="mt-3 text-indigo-500 inline-flex items-center"><NuxtLink to="/boo">Learn More</NuxtLink>
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="flex flex-wrap -m-4 text-center">
-      <div class="xl:w-1/3 md:w-1/2 p-4">
-        <div class="border border-gray-400 p-6 rounded-lg">
-          <p class="leading-relaxed">Le loader tourne en boucle.</p>
-          <button class="flex text-primary-light dark:text-primary-light  mx-auto mt-6 mb-5 bg-black border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded"> <NuxtLink to="/pedro/serveur">Go!</NuxtLink></button>
-        </div>
-      </div>
-      <div class="xl:w-1/3 md:w-1/2 p-4 text-center">
-        <div class="border border-gray-400 p-6 rounded-lg">
-          <p class="leading-relaxed">HightLight Tv.</p>
-          <button class="flex text-primary-light dark:text-primary-light mx-auto mt-6 mb-5 bg-black border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded"> <NuxtLink to="/pedro/interface/">Go!</NuxtLink></button>
-        </div>
-      </div>
-      <div class="xl:w-1/3 md:w-1/2 p-4 text-center">
-        <div class="border border-gray-400 p-6 rounded-lg">
-          <p class="leading-relaxed">Trouver une session.</p>
-          <button class="flex mx-auto mt-6 text-primary-light dark:text-primary-light mb-5 bg-black border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded"> <NuxtLink to="/pedro/ia">Go!</NuxtLink></button>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+  </section>
 </template>
