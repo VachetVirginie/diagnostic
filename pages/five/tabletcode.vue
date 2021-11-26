a3d806829f48ba92b2d94dfb10848659d87d51c5
Vivi <vachetvirginie@gmail.com>
2021-11-26 14:52:12 +0100

    fix

---
+++ pages/five/tabletcode.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="text-gray-600 body-font text-primary-dark dark:text-primary-light">
+  <div class="container px-5 py-24 mx-auto flex flex-wrap">
+    <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+      <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+      </div>
+      <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">1</div>
+      <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <p class="leading-relaxed">{{$t('_five.tablet.problems.tablet.solution.step1')}}</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+      <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+      </div>
+      <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">2</div>
+      <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <p class="leading-relaxed">{{$t('_five.tablet.problems.tablet.solution.step2')}}</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+      <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+      </div>
+      <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">3</div>
+      <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <p class="leading-relaxed">{{$t('_five.tablet.problems.tablet.solution.step3')}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
