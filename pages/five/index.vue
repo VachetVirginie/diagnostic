25fd4f55dc37bc8661029b489925f419aa47e69d
Vivi <vachetvirginie@gmail.com>
2021-12-16 16:24:36 +0100

    add component for HomeCardSection

--- pages/five/index.vue
+++ pages/five/index.vue
@@ -2,13 +2,13 @@
   <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto">
       <div class="flex flex-col text-center w-full mb-20">
-        <h2 class="sm:text-3xl text-2xl text-red tracking-widest font-medium title-font mb-1">{{$t('_five.title')}}</h2>
+        <h2 class="sm:text-3xl text-2xl text-purple-500 tracking-widest font-medium title-font mb-1">{{$t('_five.title')}}</h2>
       </div>
       <div class="flex flex-wrap -m-4">
         <div class="p-4 md:w-1/3">
           <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
             <div class="flex items-center mb-3">
-              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-red text-white flex-shrink-0">
+              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-purple-500 text-white flex-shrink-0">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                 </svg>
@@ -17,7 +17,7 @@
             </div>
             <div class="flex-grow">
               <p class="leading-relaxed text-base">{{$t('_five.loader.text')}}</p>
-              <a class="mt-3 text-red inline-flex items-center"> <NuxtLink :to="localePath('')">{{$t('_commons.learn_more')}}</NuxtLink>
+              <a class="mt-3 text-purple-500 inline-flex items-center"> <NuxtLink :to="localePath('')">{{$t('_commons.learn_more')}}</NuxtLink>
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
@@ -28,7 +28,7 @@
         <div class="p-4 md:w-1/3">
           <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
             <div class="flex items-center mb-3">
-              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-red text-white flex-shrink-0">
+              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-purple-500 text-white flex-shrink-0">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
               </svg>
@@ -37,7 +37,7 @@
             </div>
             <div class="flex-grow">
               <p class="leading-relaxed text-base">{{$t('_five.tablet.text')}}</p>
-              <a class="mt-3 text-red inline-flex items-center"><NuxtLink :to="localePath('/five/tabletcode')">{{$t('_commons.learn_more')}}</NuxtLink>
+              <a class="mt-3 text-purple-500 inline-flex items-center"><NuxtLink :to="localePath('/five/tabletcode')">{{$t('_commons.learn_more')}}</NuxtLink>
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
@@ -48,7 +48,7 @@
         <div class="p-4 md:w-1/3">
           <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
             <div class="flex items-center mb-3">
-              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-red text-white flex-shrink-0">
+              <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-purple-500 text-white flex-shrink-0">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
@@ -57,7 +57,7 @@
             </div>
             <div class="flex-grow">
               <p class="leading-relaxed text-base">{{$t('_commons.session.find_session.text')}}</p>
-              <a class="mt-3 text-red inline-flex items-center"><NuxtLink :to="localePath('/five/tabletcode')">{{$t('_commons.learn_more')}}</NuxtLink>
+              <a class="mt-3 text-purple-500 inline-flex items-center"><NuxtLink :to="localePath('/five/tabletcode')">{{$t('_commons.learn_more')}}</NuxtLink>
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
--- pages/pedro/index.vue
+++ pages/pedro/index.vue
@@ -2,13 +2,13 @@
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
-      <h2 class="sm:text-3xl text-2xl text-red tracking-widest font-medium title-font mb-1">{{$t('_pedro.title')}}</h2>
+      <h2 class="sm:text-3xl text-2xl text-purple-500 tracking-widest font-medium title-font mb-1">{{$t('_pedro.title')}}</h2>
     </div>
     <div class="flex flex-wrap -m-4">
       <div class="p-4 md:w-1/3">
         <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-red text-white flex-shrink-0">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-purple-500 text-white flex-shrink-0">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01" />
               </svg>
@@ -17,7 +17,7 @@
           </div>
           <div class="flex-grow">
             <p class="leading-relaxed text-base">{{$t('_pedro.web_server.text')}}</p>
-            <a class="mt-3 text-red inline-flex items-center"> <NuxtLink :to="localePath('')">{{$t('_commons.learn_more')}}</NuxtLink>
+            <a class="mt-3 text-purple-500 inline-flex items-center"> <NuxtLink :to="localePath('')">{{$t('_commons.learn_more')}}</NuxtLink>
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
@@ -28,7 +28,7 @@
       <div class="p-4 md:w-1/3">
         <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-red text-white flex-shrink-0">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-purple-500 text-white flex-shrink-0">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
@@ -37,7 +37,7 @@
           </div>
           <div class="flex-grow">
             <p class="leading-relaxed text-base">{{$t('_pedro.interface.text')}}</p>
-            <a class="mt-3 text-red inline-flex items-center"><NuxtLink :to="localePath('/pedro/interface/')">{{$t('_commons.learn_more')}}</NuxtLink>
+            <a class="mt-3 text-purple-500 inline-flex items-center"><NuxtLink :to="localePath('/pedro/interface/')">{{$t('_commons.learn_more')}}</NuxtLink>
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
@@ -48,7 +48,7 @@
       <div class="p-4 md:w-1/3">
         <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-red text-white flex-shrink-0">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-purple-500 text-white flex-shrink-0">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
               </svg>
@@ -57,7 +57,7 @@
           </div>
           <div class="flex-grow">
             <p class="leading-relaxed text-base">{{$t('_pedro.ia_server.text')}}</p>
-            <a class="mt-3 text-red inline-flex items-center"><NuxtLink :to="localePath('')">{{$t('_commons.learn_more')}}</NuxtLink>
+            <a class="mt-3 text-purple-500 inline-flex items-center"><NuxtLink :to="localePath('')">{{$t('_commons.learn_more')}}</NuxtLink>
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
@@ -68,7 +68,7 @@
       <div class="p-4 md:w-1/3">
         <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-red text-white flex-shrink-0">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-purple-500 text-white flex-shrink-0">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
               </svg>
@@ -77,7 +77,7 @@
           </div>
           <div class="flex-grow">
             <p class="leading-relaxed text-base">{{$t('_pedro.camera.text')}}</p>
-            <a class="mt-3 text-red inline-flex items-center"><NuxtLink :to="localePath('/pedro/camera/')">{{$t('_commons.learn_more')}}</NuxtLink>
+            <a class="mt-3 text-purple-500 inline-flex items-center"><NuxtLink :to="localePath('/pedro/camera/')">{{$t('_commons.learn_more')}}</NuxtLink>
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
@@ -88,7 +88,7 @@
             <div class="p-4 md:w-1/3">
         <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-red text-white flex-shrink-0">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-purple-500 text-white flex-shrink-0">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
             </svg>
@@ -97,7 +97,7 @@
           </div>
           <div class="flex-grow">
             <p class="leading-relaxed text-base">{{$t('_pedro.live.text')}}</p>
-            <a class="mt-3 text-red inline-flex items-center"><NuxtLink :to="localePath('/pedro/live/')">{{$t('_commons.learn_more')}}</NuxtLink>
+            <a class="mt-3 text-purple-500 inline-flex items-center"><NuxtLink :to="localePath('/pedro/live/')">{{$t('_commons.learn_more')}}</NuxtLink>
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
